289b50f3220c8b0b419b80d6a592efd7d292232e
phalgunv <phalgun251@gmail.com>
2015-12-09 10:30:25 +0530

    Updated file

--- threads/clkthreads.cpp
+++ threads/clkthreads.cpp
@@ -2,8 +2,10 @@
 #include<pthread.h>
 #include<unistd.h>
 #include<csignal>
+#include <sys/syscall.h>
 using namespace std;
 
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 class digital
 {
 	int s,m,h;
@@ -23,72 +25,76 @@ class digital
 	void M()
 	{
 		if ( m == 59)
-		H();
+			H();
 		m %= 60;
 		m++;
 	}
 
 	void S()
 	{
-		while(1)
-		{
 		s %= 60;
+		getValues();
 		if(s == 59)
-		M();
-        getValues();
+			M();
 		s++;
-		}
 	}
-    
-    void getValues(){
-        cout<<" h = "<<h<<"m = "<<m<<"s = "<<s<<endl;
-        sleep(1);
-    }
+
+	void getValues(){
+		cout<<" h = "<<h<<"m = "<<m<<"s = "<<s<<endl;
+	}
 
 };
+static int sid4;
+digital *obj = new digital(0,0,0);
+digital *obj1 = new digital(12,34,56);
+pthread_t  p_thread, p_thread2, t3, t4;       /* thread's structure                     */
 
-	digital *obj = new digital(0,0,0);
-	digital *obj1 = new digital(12,34,56);
-void*
+
+
+	void*
 do_loop(void* data)
 {
-    digital*  obj = ((digital *)data);     /* thread identifying number */
-    /* terminate the thread */
-	//cout << " "
-	obj->S();
+	digital*  obj = ((digital *)data);     /* thread identifying number */
+	while(1){	
+		cout <<"Thread id: " <<  (unsigned int)pthread_self()<<"obj pointing to "<<obj <<endl;
+		/* terminate the thread */
+		obj->S();
+		sleep(1);
+	}
 }
 
 void sighandler(int signum)
 {
 	cout << "received sigint.." << signum <<  endl;
-    static int count;
-    obj1->getValues();
-    if(count)
-    {
-        obj1 = obj;
-    cout<<"After assigning values"<<endl;
-    pthread_kill(id4);
-    obj1->getValues();
-    sleep(5);
-    }
-    if (count == 0) 
-    count++;
-//	sleep(5);
+	static int count;
+	cout<<"Before assigning values"<<endl;
+	obj1->getValues();
+	if(count)
+	{
+		// *obj1=(*obj);
+		obj1->operator=(*obj);
+		cout<<"After assigning values"<<endl;
+		obj1->getValues();
+	}
+	if (count == 0) 
+		count++;
 }
 
-    
+
 int main()
 {
+	cout <<"obj  add"<<obj<<endl;
+	cout <<"obj 1 add"<<obj1<<endl;
 	signal(SIGINT,sighandler);	   
-    pause();
-    int  thr_id, thr_id2, id3, id4;         /* thread ID for the newly created thread */
-    int t2;
-    pthread_t  p_thread, p_thread2, t3, t4;       /* thread's structure                     */
-    pthread_t p_thread4;
-
-    /* create a new thread that will execute 'do_loop()' */
-    thr_id = pthread_create(&p_thread, NULL, do_loop, (void*)obj);
-    id4 = pthread_create(&t4, NULL, do_loop, (void*)obj1);
-    pthread_exit(NULL);
-	//d.S();
+	pause();
+	int  thr_id, thr_id2, id3, id4;         /* thread ID for the newly created thread */
+	int t2;
+	pthread_t p_thread4;
+	/* create a new thread that will execute 'do_loop()' */
+	thr_id = pthread_create(&p_thread, NULL, do_loop, (void*)obj);
+	id4 = pthread_create(&t4, NULL, do_loop, (void*)obj1);
+	sid4 = id4;
+	pthread_join(p_thread,NULL);
+	pthread_join(t4,NULL);
+	pthread_exit(NULL);
 }
